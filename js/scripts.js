161c6ea9e7c4a86ba9e0b634735abfd3555607af
puci2020 <adam.pucicki97@gmail.com>
2023-08-14 17:30:20 +0200

    Pricing section

--- js/scripts.js
+++ js/scripts.js
@@ -66,6 +66,42 @@ $(function () {
             // instead of a settings object
         ],
     })
+    $('.slider_one').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ],
+    })
 
     //toggle scroll menu
     var scrollTop = 0
@@ -105,14 +141,13 @@ $(function () {
             1000,
         )
     })
-
-    $('button.navbar-toggler').on('click', function () {
-        let show = $('button.navbar-toggler').attr('aria-expanded')
+    $('button.navbar-toggler').click(function () {
+        let show = $(this).attr('aria-expanded')
         console.log(show)
-        if (show) {
+        $('.navbar-container').css('background-color', 'none')
+
+        if (show == false) {
             $('.navbar-container').css('background-color', 'black')
-        } else {
-            $('.navbar-container').css('background-color', 'none')
         }
     })
 
